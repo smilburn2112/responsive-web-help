8e3236cf24dcb8144fc46807422dce6999d5fefd
Stephen Milburn <sjmilburn12@gmail.com>
2022-06-17 12:27:21 -0500

    Add index file and related assets.

---
+++ oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,16 @@
+define(function() {var keywords=[{w:"Overview:",p:["p0"]},{w:"Linux",p:["p0","p5","p6"]},{w:"Mint",p:["p0","p5","p6"]},{w:"and",p:["p0"]},{w:"Oracle",p:["p0","p4"]},{w:"VM",p:["p0","p4"]},{w:"VirtualBox",p:["p0","p4","p7","p8"]},{w:"v.",p:["p0","p4"]},{w:"6.1.34",p:["p0","p4"]},{w:"Booting",p:["p1"]},{w:"Your",p:["p1","p2","p6"]},{w:"Virtual",p:["p1","p2","p6","p8"]},{w:"Machine",p:["p1","p2","p6","p8"]},{w:"for",p:["p1","p4"]},{w:"the",p:["p1","p5"]},{w:"First",p:["p1"]},{w:"Time",p:["p1"]},{w:"Configuring",p:["p2"]},{w:"Context",p:["p3"]},{w:"Sensitive",p:["p3"]},{w:"Help",p:["p3"]},{w:"Downloading",p:["p4","p5"]},{w:"&",p:["p4"]},{w:"Installing",p:["p4","p6","p7"]},{w:"Windows",p:["p4"]},{w:".ISO",p:["p5"]},{w:"file",p:["p5"]},{w:"on",p:["p6"]},{w:"Guest",p:["p7"]},{w:"Editions",p:["p7"]},{w:"Creating",p:["p8"]},{w:"a",p:["p8"]},{w:"New",p:["p8"]},{w:"with",p:["p8"]}];
+var ph={};
+ph["p0"]=[0, 1, 2, 3, 4, 5, 6, 7, 8];
+ph["p1"]=[9, 10, 11, 12, 13, 14, 15, 16];
+ph["p2"]=[17, 10, 11, 12];
+ph["p3"]=[18, 19, 20];
+ph["p4"]=[21, 22, 23, 4, 5, 6, 7, 8, 13, 24];
+ph["p5"]=[21, 14, 1, 2, 25, 26];
+ph["p6"]=[23, 1, 2, 27, 10, 11, 12];
+ph["p7"]=[23, 6, 28, 29];
+ph["p8"]=[30, 31, 32, 11, 12, 33, 6];
+     return {
+         keywords: keywords,
+         ph: ph
+     }
+});
